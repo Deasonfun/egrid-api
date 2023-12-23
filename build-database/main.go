96799e3fd6a1256a38d300f2236a9dc8eb351c55
Deasonfun <dftothemax@gmail.com>
2023-12-23 18:52:31 -0500

    Switched to COPY command instead of INSERT

--- build-database/main.go
+++ build-database/main.go
@@ -1,50 +1,69 @@
 package main
 
 import (
-	"database/sql"
+	"context"
 	"fmt"
 	"log"
 	"os"
+	"path/filepath"
 	"strings"
 
+	"github.com/jackc/pgx/v5"
 	"github.com/joho/godotenv"
-	_ "github.com/lib/pq"
 )
 
-func CreateTable(tableName string, db *sql.DB, headers []string) {
-	_, err := db.Exec(fmt.Sprintf("DROP TABLE IF EXISTS %s", tableName))
+func CreateTable(tableName string, db *pgx.Conn, headers []string) {
+	tx, err := db.Begin(context.Background())
+	CheckError(err)
+	_, err = tx.Exec(context.Background(), fmt.Sprintf("DROP TABLE IF EXISTS %s", tableName))
 	CheckError(err)
 
 	createTable := fmt.Sprintf(
-		"CREATE TABLE IF NOT EXISTS %s(id SERIAL PRIMARY KEY, ", tableName,
+		"CREATE TABLE IF NOT EXISTS %s(", tableName,
 	)
-	for _, header := range headers {
-		headerInsertPart := fmt.Sprintf("%s VARCHAR(255), ", header)
+	for h := range headers {
+		headerInsertPart := fmt.Sprintf("%s VARCHAR(255), ", headers[h])
 		createTable = createTable + headerInsertPart
 	}
 	createTable = strings.TrimSuffix(createTable, ", ")
 	createTable = createTable + ")"
-	_, err = db.Exec(createTable)
+	_, err = db.Exec(context.Background(), createTable)
+	CheckError(err)
+	err = tx.Commit(context.Background())
 	CheckError(err)
 }
 
-func InsertData(db *sql.DB, data []string, tableName string, err error) {
-	//println("%s", data)
+func CopyTable(db *pgx.Conn, filepath string, fileName string) {
+	tx, err := db.Begin(context.Background())
+	CheckError(err)
+	copyDataString := fmt.Sprintf("COPY %s FROM '%s' csv header", fileName, filepath)
+	_, err = tx.Exec(context.Background(), copyDataString)
+	CheckError(err)
+	err = tx.Commit(context.Background())
+	CheckError(err)
+
+}
+
+func InsertData(db *pgx.Conn, data []string, tableName string, err error) {
+	tx, err := db.Begin(context.Background())
+	CheckError(err)
 	var rowValues []string
-	for _, row := range data {
+	for row := range data {
 		rowValues = nil
-		row := strings.Split(row, ",")
-		for _, cell := range row {
-			cell := fmt.Sprintf("'%s'", cell)
+		row := strings.Split(data[row], ",")
+		for cell := range row {
+			cell := fmt.Sprintf("'%s'", row[cell])
 			rowValues = append(rowValues, cell)
 		}
 		values := strings.Join(rowValues, ",")
 		populateCell := fmt.Sprintf("INSERT INTO %s VALUES(DEFAULT, %s) RETURNING *",
 			tableName, values)
 
-		_, err = db.Exec(populateCell)
+		_, err = tx.Exec(context.Background(), populateCell)
 		CheckError(err)
 	}
+	err = tx.Commit(context.Background())
+	CheckError(err)
 }
 
 func main() {
@@ -64,12 +83,12 @@ func main() {
 		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
 		host, port, user, password, dbname)
 
-	db, err := sql.Open("postgres", connectString)
-
+	//db, err := sql.Open("postgres", connectString)
+	db, err := pgx.Connect(context.Background(), connectString)
 	CheckError(err)
 
-	defer db.Close()
-	err = db.Ping()
+	defer db.Close(context.Background())
+	err = db.Ping(context.Background())
 	CheckError(err)
 	fmt.Println("Connnected to database...")
 
@@ -81,7 +100,6 @@ func main() {
 			continue
 		}
 		folder := fmt.Sprintf("./data/%s", yearFolders.Name())
-		fmt.Printf("%s\n", folder)
 		files, err := os.ReadDir(folder)
 		CheckError(err)
 		for _, files := range files {
@@ -91,18 +109,24 @@ func main() {
 			fileDir := fmt.Sprintf("%s/%s", folder, files.Name())
 			file, err := os.ReadFile(fileDir)
 			CheckError(err)
+			absFilepath, err := filepath.Abs(fileDir)
+			CheckError(err)
 			var fileContent = string(file[:])
 			var splitFile = strings.SplitAfterN(fileContent, "\n", 2)
 			headers := strings.Split(splitFile[0], ",")
 			fileName := strings.Split(files.Name(), ".")
-			fmt.Println(fileName[0])
 
-			data := strings.Split(splitFile[1], "\n")
+			//data := strings.Split(splitFile[1], "\n")
 
 			CreateTable(fileName[0], db, headers)
 
-			InsertData(db, data, fileName[0], err)
+			fmt.Println(fileDir)
 
+			CopyTable(db, absFilepath, fileName[0])
+
+			//fmt.Println("Inserting... ;)")
+			//InsertData(db, data, fileName[0], err)
+			//fmt.Println("Finished... ;)")
 		}
 	}
 }
